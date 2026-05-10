81ab83cf5fe532fb3ce9a3cc8a60377c895174bd
BGM <bhalaganapathym@gmail.com>
2026-05-10 13:11:42 +0530

    second commit

---
+++ api/index.py
@@ -0,0 +1,4 @@
+from main import app
+
+# Vercel requires the FastAPI instance to be named `app` and located in the `api` directory
+# so that it can be automatically detected as a Serverless Function.
