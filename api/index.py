f4465b7d845ea31dd07924d788a282b4c607dac7
BGM <bhalaganapathym@gmail.com>
2026-05-10 13:14:35 +0530

    third commit

--- api/index.py
+++ api/index.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Append the project root to sys.path so we can import main.py from the parent directory
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from main import app
 
 # Vercel requires the FastAPI instance to be named `app` and located in the `api` directory
